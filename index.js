860e07639d7715b368ed810f61c55985120e6519
Wilsonm123_@hotmail.com <wilsonm123_@hotmail.com>
2024-08-27 21:32:16 -0500

    HTML, BOTONES

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const port = 3000;
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta para servir el index.html
+// Servir el archivo index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
 });
-// Ruta para servir el index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html', 'pagos.html'));
+
+// Manejo de rutas dinámicas para otros archivos HTML
+app.get('/:filename', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'html', req.params.filename);
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            res.status(404).send('Archivo no encontrado');
+        }
+    });
 });
 
+// Escuchar en el puerto especificado
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
